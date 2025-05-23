eedf34593b51634c6d3c5e0222906e45c0108d91
kenzu <148296628+Kenzxyx@users.noreply.github.com>
2025-05-23 12:51:26 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Navbar from "./components/Navbar"
-import Hero from "./components/hero"
+import Hero from "./components/Hero"
 import Posts from "./components/Posts"
  import Footer from "./components/Footer"
 
